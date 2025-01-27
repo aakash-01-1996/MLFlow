7ace66a1a332c531e3d004c243c6077a4af71e51
aakash-01-1996 <aakashambodkar99@gmail.com>
2025-01-26 20:07:01 -0700

    Nested_Runs, custom_models & model_Schema

--- 01_create_experiment.py
+++ 01_create_experiment.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     
     # create a new mlflow experiment
     experiment_id = mlflow.create_experiment(
-        name="testing_mlflow7",
+        name="testing_mlflow8",
         artifact_location="testing_mlflow1_artifacts",
         tags={"env": "dev", "version": "1.0.0"},
     )
